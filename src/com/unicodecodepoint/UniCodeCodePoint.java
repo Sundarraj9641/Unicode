f1388e6ea10c66f76a68340fc4366e29b2cfb051
Mahalingam <sundarraj.mahalingam@capgemini.com>
2024-05-19 11:18:49 +0530

    pushed by sundarraj

---
+++ src/com/unicodecodepoint/UniCodeCodePoint.java
@@ -0,0 +1,14 @@
+package com.unicodecodepoint;
+
+public class UniCodeCodePoint {
+
+	public static void main(String[] args) {
+		
+		String str = "Capgemini";
+		
+		int in = str.codePointAt(4);
+		System.out.println(in);
+
+	}
+
+}
